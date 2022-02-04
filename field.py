b2e78cd35147413780d86f0fe9f557cafd2a2044
Oscar Sierra <oasierra5@misena.edu.co>
2022-02-04 13:38:30 -0500

    Solicionamos elproblemaen la clase field

--- field.py
+++ field.py
@@ -3,19 +3,7 @@ class Field:
     def __init__(self):
         self.homeless_coordinates = {}
             
-    def add_homeless(self, homeclass Coordenada:
-        
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        
-    def mover(self, delta_x, delta_y):
-        return Coordenada(self.x + delta_x, self.y + delta_y)
-    
-    def distancia(self, otra_coordenada):
-        delta_x = self.x - otra_coordenada.x
-        delta_y = self.y - otra_coordenada.y
-        
+    def add_homeless(self, homeless, coordinate):
         self.homeless_coordinates[homeless] = coordinate
         
     def move_homeless(self, homeless):
